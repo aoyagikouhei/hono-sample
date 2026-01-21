9cbce56917fce438232149e75abad6c805dfe67c
aoyagikouhei <aoyagi.kouhei@gmail.com>
2026-01-21 12:34:32 +0900

    add vite

--- README.md
+++ README.md
@@ -10,4 +10,7 @@ curl -fsSL https://get.pnpm.io/install.sh | sh -
 mkdir apps
 cd apps
 pnpm create hono@latest ./backend --template nodejs --pm pnpm --install
+pnpm i -D vite @hono/vite-dev-server
+
+npm run dev
 ```
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -7,9 +7,13 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+/*
 serve({
   fetch: app.fetch,
   port: 3000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
+*/
+
+export default app
