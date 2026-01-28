364312ae449670b9f218075da34319a3fdf37697
aoyagikouhei <aoyagi.kouhei@gmail.com>
2026-01-28 09:37:49 +0900

    made rust

--- apps/backend/src/generate.ts
+++ apps/backend/src/generate.ts
@@ -4,11 +4,21 @@ import ejs from 'ejs';
 
 import { UserSchema, UsersListResponseSchema, ErrorResponseSchema } from './schemas/user'
 
+const columns = []
+for (const [key, schema] of Object.entries(UserSchema.shape)) {
+    columns.push({
+        name: key,
+        type: schema.type === 'optional' ? schema.def.innerType.type : schema.type,
+        optional: schema.type === 'optional',
+        schema: schema,
+     });
+}
+
 const renderd = ejs.render(fs.readFileSync('./templates/rust.ejs', 'utf-8'), {
-  src: UserSchema,
-  _: _,
+    src: UserSchema,
+    _: _,
+    columns: columns,
 });
-console.log(UserSchema)
 
 const filename: string = `./generated/${_.snakeCase(UserSchema.metadata.name)}.rs`
 await fs.promises.writeFile(filename, renderd, { encoding: "utf-8" });
